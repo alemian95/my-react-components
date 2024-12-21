2892d8d97440f03cd31140d980fc0487d1f7e7cd
Alessandro Mian <alessandromian95@gmail.com>
2024-12-21 12:02:02 +0100

    base layout

--- app/layouts/WebLayout.tsx
+++ app/layouts/WebLayout.tsx
@@ -6,9 +6,10 @@ export const WebLayout = (props: PropsWithChildren) => {
     return (
         <SidebarProvider>
             <WebSidebar />
-            <main>
-                <SidebarTrigger />
-                {props.children}
+            <main className="min-h-dvh flex flex-col w-full">
+                <header><SidebarTrigger /></header>
+                <section className="flex-1">{props.children}</section>
+                <footer className="text-sm text-center p-1 text-muted"><span>&copy;{new Date().getFullYear()} - My React Components</span></footer>
             </main>
         </SidebarProvider>
     )
