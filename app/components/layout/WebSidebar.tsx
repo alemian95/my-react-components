a57e8bb6826f93394fe7012f81f86f512076a5a0
Alessandro Mian <alessandromian95@gmail.com>
2024-12-22 12:44:45 +0100

    sidebar links

--- app/components/layout/WebSidebar.tsx
+++ app/components/layout/WebSidebar.tsx
@@ -1,5 +1,6 @@
-import { LayoutDashboard } from "lucide-react"
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar"
+import { Calendar1, LayoutDashboard } from "lucide-react"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar"
+import { Link } from "react-router"
 
 export const WebSidebar = () => {
     return (
@@ -8,21 +9,34 @@ export const WebSidebar = () => {
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton className="hover:text-white" size="lg" asChild>
-                            <a href="#">
+                            <Link to="/">
                                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
                                     <LayoutDashboard className="w-8 h-8" />
                                 </div>
                                 <div className="flex flex-col gap-0.5 leading-none">
                                     <h1 className="font-semibold">Reusable React Components</h1>
                                 </div>
-                            </a>
+                            </Link>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+                <SidebarGroup>
+                    <SidebarGroupLabel>Components</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton asChild isActive={false}>
+                                    <Link to="/components/calendar">
+                                        <Calendar1 />
+                                        <span>Calendar</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
